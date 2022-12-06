657f87db6da7f89e9588e83e8bf9177b78ead11c
Ali <ssendamevil@gmail.com>
2022-12-06 22:28:14 +0600

    Pratice task 1 and 2

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 "use strict";
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log('s'*9);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+if(personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count <30){
+    alert('Вы классический зритель');
+}else if(personalMovieDB.count >=30){
+    alert('Вы киноман');
+}else{
+    alert('Произошла ошибка');
+}
+
+
+for(let i = 0; i<2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b!=null && a!='' && b!='' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }else{
+        i--;
+        console.log('error');
+    }
+    
+}
+
+
+
+console.log(personalMovieDB);
